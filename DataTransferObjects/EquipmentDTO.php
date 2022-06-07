5c67f4915a2d7741c5bb66a9cd30a7f4233eca3e
renato_sans <renato@datacopy.com.br>
2022-06-07 01:04:47 -0300

    Ajustes

--- DataTransferObjects/EquipmentDTO.php
+++ DataTransferObjects/EquipmentDTO.php
@@ -1,37 +1,37 @@
 <?php
 
 class EquipmentDTO{
-    var $insID               = 0;
-    var $manufacturerSN      = "";
-    var $internalSN          = "";
-    var $itemCode            = "";
-    var $itemName            = "";
-    var $customer            = "";
-    var $custmrName          = "";
-    var $contactPerson       = 0;
-    var $addressType         = "";
-    var $street              = "";
-    var $streetNo            = "";
-    var $building            = "";
-    var $zip                 = "";
-    var $block               = ""; 
-    var $city                = "";
-    var $state               = "";
-    var $country             = "";
-    var $instLocation        = "";
-    var $status              = "";
-    var $installationDate    = NULL;
-    var $installationDocNum  = 0;
-    var $counterInitialVal   = 0;
-    var $removalDate         = NULL;
-    var $removalDocNum       = 0;
-    var $counterFinalVal     = 0;
-    var $technician          = 0;
-    var $model               = 0;
-    var $capacity            = 0;
-    var $sla                 = 0;
-    var $comments            = "";
-    var $salesPerson         = 0;
+    public $insID               = 0;
+    public $manufacturerSN      = "";
+    public $internalSN          = "";
+    public $itemCode            = "";
+    public $itemName            = "";
+    public $customer            = "";
+    public $custmrName          = "";
+    public $contactPerson       = 0;
+    public $addressType         = "";
+    public $street              = "";
+    public $streetNo            = "";
+    public $building            = "";
+    public $zip                 = "";
+    public $block               = ""; 
+    public $city                = "";
+    public $state               = "";
+    public $country             = "";
+    public $instLocation        = "";
+    public $status              = "";
+    public $installationDate    = NULL;
+    public $installationDocNum  = 0;
+    public $counterInitialVal   = 0;
+    public $removalDate         = NULL;
+    public $removalDocNum       = 0;
+    public $counterFinalVal     = 0;
+    public $technician          = 0;
+    public $model               = 0;
+    public $capacity            = 0;
+    public $sla                 = 0;
+    public $comments            = "";
+    public $salesPerson         = 0;
 
 
     function __construct(){
