b58a60eea7a32bf5063153a413f857c23ba8c5af
renato_sans <renato@datacopy.com.br>
2022-06-07 00:27:13 -0300

    Ajustes

--- lib/db/sqlServerMock.sql
+++ lib/db/sqlServerMock.sql
@@ -7,16 +7,16 @@ CREATE TABLE OINS(
  ItemName           VARCHAR(255) NULL,
  Customer           INT NULL,
  CustmrName         VARCHAR(255) NULL,
- contactPerson      INT NULL,
- addressType        INT NULL,
- street             VARCHAR(255) NULL,
- streetNo           VARCHAR(255) NULL,
- building           VARCHAR(255) NULL,
- zip                VARCHAR(255) NULL,
- block              VARCHAR(255) NULL,
- city               VARCHAR(255) NULL,
- state              VARCHAR(255) NULL,
- country            VARCHAR(255) NULL,
+ ContactPerson      INT NULL,
+ AddressType        INT NULL,
+ Street             VARCHAR(255) NULL,
+ StreetNo           VARCHAR(255) NULL,
+ Building           VARCHAR(255) NULL,
+ Zip                VARCHAR(255) NULL,
+ Block              VARCHAR(255) NULL,
+ City               VARCHAR(255) NULL,
+ State              VARCHAR(255) NULL,
+ Country            VARCHAR(255) NULL,
  instLocation       VARCHAR(255) NULL,
  status             CHAR(2) NULL,
  U_InstallationDate DATE NULL,
@@ -36,3 +36,4 @@ CREATE TABLE OINS(
 SELECT * FROM OINS
 SELECT manufSN, status, COUNT(1) quantidade FROM OINS GROUP BY manufSN, status HAVING COUNT(1) > 1 AND status = 'A'
 SELECT * FROM OINS WHERE (status = 'A' OR status = 'L') AND U_InstallationDate > GETDATE() ORDER BY manufSN
+UPDATE OINS SET U_InstallationDate = GETDATE(), U_InstallationDocNum = '', U_BwPageCounter = 123456, U_RemovalDate = GETDATE(), U_RemovalDocNum = '', U_BwPageCounter2 = 123456, U_Technician = 123, U_Model = 123, U_Capacity = '', U_SLA = '', U_Comments = '', U_SalesPerson = 123 WHERE InsId = 1
