9e23dabbae919412d481c2519a4a59d975f97ff6
renato_sans <renato@datacopy.com.br>
2022-06-17 11:24:44 -0300

    Ajustes

--- DataAccessObjects/ManufacturerDAO.php
+++ DataAccessObjects/ManufacturerDAO.php
@@ -12,6 +12,30 @@ class ManufacturerDAO{
     }
 
     function RetrieveRecordArray($filter = null){
+        $dtoArray = array();
+
+        $query = "SELECT * FROM fabricante WHERE ".$filter.";";
+        if (empty($filter)) $query = "SELECT * FROM fabricante;";
+
+        $recordSet = mysqli_query($this->mysqlConnection, $query);
+        if ((!$recordSet) && ($this->showErrors)) {
+            print_r(mysqli_error());
+            echo '<br/><br/>';
+        }
+        $recordCount = mysqli_num_rows($recordSet);
+        if ($recordCount == 0) return $dtoArray;
+
+        $index = 0;
+        while( $record = mysqli_fetch_array($recordSet) ){
+            $dto = new ManufacturerDTO();
+            $dto->id       = $record['id'];
+            $dto->nome     = $record['nome'];
+
+            $dtoArray[$index] = $dto;
+            $index++;
+        }
+        mysqli_free_result($recordSet);
+
         return $dtoArray;
     }
 
