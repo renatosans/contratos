b3a82ad356967df66ea38ca52aedad8912df5514
renato_sans <renato@datacopy.com.br>
2022-06-17 11:09:30 -0300

    Ajustes

--- DataAccessObjects/ManufacturerDAO.php
+++ DataAccessObjects/ManufacturerDAO.php
@@ -12,23 +12,6 @@ class ManufacturerDAO{
     }
 
     function RetrieveRecordArray($filter = null){
-        $dtoArray = array();
-
-        $query = "SELECT FirmCode, FirmName FROM OMRC WHERE ".$filter;
-        if (empty($filter)) $query = "SELECT FirmCode, FirmName FROM OMRC ORDER BY FirmCode ASC";
-        $recordSet = sqlsrv_query($this->sqlserverConnection, $query);
-
-        $index = 0;
-        while( $record = sqlsrv_fetch_array($recordSet, SQLSRV_FETCH_ASSOC) ){
-            $dto = new ManufacturerDTO();
-            $dto->FirmCode = $record["FirmCode"];
-            $dto->FirmName = $record["FirmName"];
-
-            $dtoArray[$index] = $dto;
-            $index++;
-        }
-        sqlsrv_free_stmt($recordSet);
-
         return $dtoArray;
     }
 
