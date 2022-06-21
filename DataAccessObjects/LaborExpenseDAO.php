96e58c3cb485c237f5cea8cbc881dbcc15458a64
renato_sans <renato@datacopy.com.br>
2022-06-21 08:30:30 -0300

    Correção de bug na consulta usando linked server

--- DataAccessObjects/LaborExpenseDAO.php
+++ DataAccessObjects/LaborExpenseDAO.php
@@ -20,11 +20,11 @@ class LaborExpenseDAO {
         $query  = "SELECT * FROM ( ";
         $query .= "SELECT EQP.customer AS codigoCliente, CLI.cardName + ' (' + CLI.cardCode + ')' AS nomeCliente, EQP.insID AS codigoEquipamento, EQP.manufSN AS serieEquipamento, MDL.id AS codigoModelo, MDL.modelo AS tagModelo, ";
         $query .= "FAB.nome AS fabricante, CHAM.id AS numeroChamado, CHAM.tempoAtendimento, MONTH(CHAM.dataAtendimento) AS mesReferencia, YEAR(CHAM.dataAtendimento) AS anoReferencia ";
-        $query .= "FROM MYSQL...chamadoServico CHAM ";
+        $query .= "FROM MYSQL.addoncontratos..chamadoServico CHAM ";
         $query .= "JOIN OINS EQP ON CHAM.cartaoEquipamento = EQP.insID ";
         $query .= "JOIN OCRD CLI ON EQP.customer = CLI.cardCode ";
-        $query .= "JOIN MYSQL...modeloEquipamento MDL ON EQP.U_Model = MDL.id ";
-        $query .= "JOIN MYSQL...fabricante FAB ON MDL.fabricante = FAB.id ";
+        $query .= "JOIN MYSQL.addoncontratos..modeloEquipamento MDL ON EQP.U_Model = MDL.id ";
+        $query .= "JOIN MYSQL.addoncontratos..fabricante FAB ON MDL.fabricante = FAB.id ";
         $query .= "              ) LABOREXPENSES ";
         if (isset($filter) && (!empty($filter))) $query = $query." WHERE ".$filter;
 
